a2f13f42453e6c476d3d8e7ad4100f5dd527d852
Alex's repository <alexneedham33@gmail.com>
2020-02-16 14:11:50 +0000

    working first business

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+import Business from '../business/business.js';
+
+function App() {
+  return (
+    <div  id="App_id" className="App">
+      <Business />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/business_list/business_list.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Business from '../business/business';
+
